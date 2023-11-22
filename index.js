360a581c187cec96ee91919884d86aa9de51792b
cgreg714 <c.gregoire714@gmail.com>
2023-11-21 22:23:31 -0500

    tickets 1-3 complete, began work on ticket 4

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 //! Global Variables
 const baseURL = "https://fakestoreapi.com/products/";
-const eURL = "https://fakestoreapi.com/products/category/electronics";
-const jURL ="https://fakestoreapi.com/products/category/jewelery";
-const wURL = "https://fakestoreapi.com/products/category/women's%20clothing?sort=asc"; //this is an invalid url
-const mURL = "https://fakestoreapi.com/products/category/men's%20clothing?sort=asc";  //so is this, but where are these items held besides the main 
 const electronics = document.querySelector('#electronics');
 const jewelry = document.querySelector('#jewelry');
 const womensClothing = document.querySelector('#womensClothing');
 const mensClothing = document.querySelector('#mensClothing');
+const display = document.getElementById("display")
+const cart = []
 //empty array for cart
 
 const fakeStore = async (endpoint) => {
-/*  fetch(endpoint)
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.log(error));*/
   let results = await fetch(baseURL + endpoint)
-  //console.log(results)
   let capturedResults = await results.json()
-  console.log(capturedResults)
+  displayCards(capturedResults)
 }
 
 //! Event Listeners
@@ -46,7 +39,67 @@ mensClothing.addEventListener("click", e => {
     fakeStore(`category/men's%20clothing?sort=asc`)
   });
 
+//! Cards Time
 
+function displayCards(items) { //invoke function after the fetch results in the fakeStore
+  items.forEach(item => {
+    let title = document.createElement("h5")
+    title.className = "card-title";
+    title.textContent = item.title;
+    card.appendChild(title);
+    console.log(item.title)
+  })
+};
+
+//* Create Elements
+let card = document.createElement("div");
+let img = document.createElement("img");
+let body = document.createElement("div");
+let title = document.createElement("h5");
+let desc = document.createElement("p");
+let btn = document.createElement("a");
+
+//* Set Attributes -- *building the single card div in our JS
+card.className = "card";
+card.style.width = "18rem";
+
+img.src = item.img;
+img.className = "card-img-top"
+img.alt = item.title;
+
+body.className = "card-body";
+
+title.className = "card-title";
+title.textContent = item.title;
+
+desc.className = "card-text";
+desc.textContent = item.description;
+
+btn.className = "btn btn-primary";
+btn.textContent = "Add to Cart";
+btn.onclick = () => {
+  //anonymous function that generates new object holding keys of: id, title, cost, quantity
+  //each key is assigned the value of the current item, with quantity set to '1'
+  //invokes submitToCart function
+}
+  
+// ????
+
+
+//* Attach Elements -- build from inside out
+
+body.appendChild(title);
+body.appendChild(btn);
+body.appendChild(desc);
+
+card.appendChild(img);
+card.appendChild(body);
+
+// ????
+
+function submitToCart(item){
+//declarative function add 'item' to 'cart' array
+};
 
   window.onload = function() {
     fakeStore('');  //add &limit=(# of items) if you need to limit the number loaded.
