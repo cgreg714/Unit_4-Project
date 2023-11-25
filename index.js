67d4a1b2a5e888e1e96904ae513f4f11cd72d95d
cgreg714 <c.gregoire714@gmail.com>
2023-11-25 14:33:51 -0500

    totally lost at the moment. async function is broken. cards not displaying. console giving multiple erros depending on lines of code commented out.

--- index.js
+++ index.js
@@ -1,90 +1,30 @@
 //! Global Variables/DOM Elements
 const baseURL = "https://fakestoreapi.com/products/";
-const electronics = document.querySelector('#electronics');
-const jewelry = document.querySelector('#jewelry');
-const womensClothing = document.querySelector('#womensClothing');
-const mensClothing = document.querySelector('#mensClothing');
-const display = document.getElementById("display")
-const cart = [] //empty array for cart
+const cart = []; //empty array for cart
 
 
-const fakeStore = async (endpoint) => {
-  let results = await fetch(baseURL + endpoint)
-  let capturedResults = await results.json()
-  displayCards(capturedResults)
-}
-console.log(fakeStore);
-//! Event Listeners
-electronics.addEventListener("click", e => {
-    e.preventDefault();
-    fakeStore('category/electronics')
-});
 
-  jewelry.addEventListener("click", e => {
-    e.preventDefault();
-    fakeStore('category/jewelery')
-  });
 
-womensClothing.addEventListener("click", e => {
-    e.preventDefault();
-    fakeStore(`category/women's%20clothing?sort=asc`)
-  });
+ //! DOM Elements
+ const electronicsNavLink = document.querySelector("#electronics");
+ const jewelryNavLink = document.querySelector('#jewelry');
+ const womenNavLink = document.querySelector('#womensClothing');
+ const menNavLink = document.querySelector('#mensClothing');
+ const display = document.getElementById("display");
+ const item = document.querySelector(".item");
 
-mensClothing.addEventListener("click", e => {
-    e.preventDefault();
-    fakeStore(`category/men's%20clothing?sort=asc`)
-  });
+//! Cards
 
-//! Cards Time
-
-function displayCards(items) { //invoke function after the fetch results in the fakeStore
+function displayItems(items) { //invoke function after the fetch results in the fakeStore
   items.forEach(item => {
   
-    let card = document.createElement("div");
-    card.classList.add('card');
-
-    let body = document.createElement("div");
-    body.classList.add('card-body');
-
-    let img = document.createElement("img");
-    img.classList.add('card-img-top');
-    img.setAttribute('src', item.img);
-    img.setAttribute('alt', item.title);
-
-    let title = document.createElement("h5");
-    title.classList.add('card-title');
-    title.textContent = item.title;
-
-    let desc = document.createElement("p");
-    desc.classList.add('desc');
-    desc.textContent = item.description;
-
-    /*let button = document.createElement("a");
-    button.classList.add('btn btn-primary'); 
-    button.textContent = 'Add to Cart'
-    button.onclick = () => {
-      cart.push(item);
-      shoppingCart();
-      }*/
-    
-    body.appendChild(img);
-    body.appendChild(title);
-    body.appendChild(desc);
-    //body.appendChild(button);
-
-    card.appendChild(body);
-
-    console.log(items)
-})};
-
-/*
-//* Create Elements
+    //* Create Elements
 let card = document.createElement("div");
 let img = document.createElement("img");
 let body = document.createElement("div");
 let title = document.createElement("h5");
 let desc = document.createElement("p");
-let btn = document.createElement("a");
+//let btn = document.createElement("a");
 
 //* Set Attributes -- *building the single card div in our JS
 card.className = "card";
@@ -101,38 +41,119 @@ title.textContent = item.title;
 
 desc.className = "card-text";
 desc.textContent = item.description;
-
+/*
 btn.className = "btn btn-primary";
 btn.textContent = "Add to Cart";
 btn.onclick = () => {
   cart.push(item);
   shoppingCart();
-
   //anonymous function that generates new object holding keys of: id, title, cost, quantity
   //each key is assigned the value of the current item, with quantity set to '1'
   //invokes submitToCart function
-}
+}*/
  
-// ????
-
-
 //* Attach Elements -- build from inside out
 
 body.appendChild(title);
-body.appendChild(btn);
+//body.appendChild(btn);
 body.appendChild(desc);
 
 card.appendChild(img);
 card.appendChild(body);
 
-div.appendChild(card);
+//item.appendChild(card);  //
 // ????
-*/
-function submitToCart(item){
+})}
+
+
+//function submitToCart(item){
 //declarative function add 'item' to 'cart' array
-};
+;
 
   window.onload = function() {
     fakeStore('');  //add &limit=(# of items) if you need to limit the number loaded.
     };
   
+
+    //! Event Listeners
+const fakeStore = async (endpoint) => {
+  let res = await fetch(baseURL + endpoint)
+  let newRes = await res.json()
+  displayItems(newRes)
+  let item = newRes.item;
+  console.log(newRes[0])//displays values from first entry of array
+  
+  let obj = {
+    title: newRes.title,
+    price: newRes.price,
+    category: newRes.category,
+    desc: newRes.description,
+    img: newRes.image
+  };
+
+  console.log(obj)  // returns values as 'undefined' ????
+  //displayItems(obj);
+ }
+
+ electronics.addEventListener("click", e => {
+  e.preventDefault();
+  fakeStore('category/electronics')
+  });
+
+jewelry.addEventListener("click", e => {
+  e.preventDefault();
+  fakeStore('category/jewelery')
+});
+
+womensClothing.addEventListener("click", e => {
+  e.preventDefault();
+  fakeStore(`category/women's%20clothing?sort=asc`)
+});
+
+mensClothing.addEventListener("click", e => {
+  e.preventDefault();
+  fakeStore(`category/men's%20clothing?sort=asc`)
+});
+
+
+
+/*
+let card = document.createElement("div");
+card.classList.add('card');
+
+let body = document.createElement("div");
+body.classList.add('card-body');
+
+let img = document.createElement("img");
+img.classList.add('card-img-top');
+img.setAttribute('src', item.image);
+img.setAttribute('alt', item.title);
+
+let title = document.createElement("h5");
+title.classList.add('card-title');
+title.textContent = item.title;
+
+let desc = document.createElement("p");
+desc.classList.add('desc');
+desc.textContent = item.description;
+
+let button = document.createElement("a");
+button.classList.add('btn btn-primary'); 
+button.textContent = 'Add to Cart'
+button.onclick = () => {
+  cart.push(item);
+  shoppingCart();
+  }
+
+
+body.appendChild(title);
+body.appendChild(desc);
+body.appendChild(button);
+
+card.appendChild(img);
+card.appendChild(body);
+
+item.appendChild(card);
+
+console.log(item)
+})};*/
